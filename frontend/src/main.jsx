aa1bd082552eee7f7a31b4c55fc5eb57dc72eb05
pulkit161001 <pulkit161001@gmail.com>
2026-01-13 21:06:29 +0530

    frontend tools setup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { ClerkProvider } from "@clerk/clerk-react";
+import { BrowserRouter } from "react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
@@ -11,10 +13,18 @@ if (!PUBLISHABLE_KEY) {
 	throw new Error("Missing Clerk Publishable Key");
 }
 
+// this is to use tanstack funtionality (which is to fetch data again)
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
-		<ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-			<App />
-		</ClerkProvider>
+		<BrowserRouter>
+			{/* after wrapping we can use tanstack functionality in our entire app */}
+			<QueryClientProvider client={queryClient}>
+				<ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+					<App />
+				</ClerkProvider>
+			</QueryClientProvider>
+		</BrowserRouter>
 	</StrictMode>
 );
